5a9afc5271e713273e7c7a9167df27c5335ef604
Pedro Medeiros <pmedeiros@doximity.com>
2025-01-07 11:40:43 -0300

    add nvim tree toggle

--- init.lua
+++ init.lua
@@ -51,5 +51,6 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input("Grep >") })
 end)
+vim.keymap.set('n', '<leader>pt', vim.cmd.NvimTreeToggle)
 
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
