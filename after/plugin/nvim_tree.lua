9b1cb14ff986271b4f7738350ed39797fa261b75
Pedro Medeiros <pedrosnk@gmail.com>
2026-01-24 16:34:09 -0500

    Add nvim-tree file explorer
    Closes #8

---
+++ after/plugin/nvim_tree.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<leader>pt', '<cmd>NvimTreeToggle<cr>', {})
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeFocus<cr>', {})
---
+++ lua/pedrosnk/lazy_plugins/nvim_tree.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  cmd = { "NvimTreeToggle", "NvimTreeFocus", "NvimTreeOpen" },
+  opts = {
+    view = {
+      width = 30,
+    },
+    renderer = {
+      icons = {
+        show = {
+          file = true,
+          folder = true,
+          folder_arrow = true,
+          git = true,
+        },
+      },
+    },
+    filters = {
+      dotfiles = false,
+    },
+  },
+}
