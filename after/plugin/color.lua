c2dc364bc999fd0ce3527bd2e56671afd0e26cf8
Pedro Medeiros <pedrosnk@gmail.com>
2026-01-24 10:42:49 -0500

    Set lazyvim with tokyonight

---
+++ after/plugin/color.lua
@@ -0,0 +1,6 @@
+local color = "tokyonight-moon"
+vim.cmd.colorscheme(color)
+
+-- set background-color
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
