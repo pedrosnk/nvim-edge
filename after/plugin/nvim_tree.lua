2327cb9eaaa1e319c420c976161b0e7ef9142dd7
Pedro Medeiros <pedrosnk@gmail.com>
2026-01-24 16:35:26 -0500

    Open nvim-tree when opening a directory

--- after/plugin/nvim_tree.lua
+++ after/plugin/nvim_tree.lua
@@ -1,2 +1,13 @@
 vim.keymap.set('n', '<leader>pt', '<cmd>NvimTreeToggle<cr>', {})
 vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeFocus<cr>', {})
+
+-- Open nvim-tree when opening a directory
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function(data)
+    local is_directory = vim.fn.isdirectory(data.file) == 1
+    if is_directory then
+      vim.cmd.cd(data.file)
+      require("nvim-tree.api").tree.open()
+    end
+  end,
+})
