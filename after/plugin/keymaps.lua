8f7e47fc5134bcb413aa33b669ef5fb324eac29b
Pedro Medeiros <pedrosnk@gmail.com>
2026-03-07 13:35:59 -0500

    center the keymaps on keymaps.lua

---
+++ after/plugin/keymaps.lua
@@ -0,0 +1,11 @@
+-- Global keymaps not tied to any specific plugin
+
+-- Clear search highlight on <Esc> in normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Window navigation with CTRL+<hjkl>
+-- See `:help wincmd` for a list of all window commands
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
