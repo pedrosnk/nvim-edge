7bebc32709860a9bd31de0aafe7a80042f56a59c
Pedro Medeiros <pedrosnk@gmail.com>
2026-05-03 22:07:18 -0400

    Add lua LSP dependency directly using native nvim config

--- lua/pedrosnk/init.lua
+++ lua/pedrosnk/init.lua
@@ -1,8 +1,8 @@
 require('pedrosnk.vim_opt')
+require('pedrosnk.lsp')
 require('pedrosnk.lazy')
 
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { '<filetype>' },
   callback = function() vim.treesitter.start() end,
 })
-
